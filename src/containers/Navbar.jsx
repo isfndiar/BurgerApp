bfaf1e41a8bf7f63456b6e7c739a16aaed3784e2
isfandiar Adi <141855809+isfndiar@users.noreply.github.com>
2024-01-07 17:15:49 +0700

    fix: navbar css to tailwind

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -6,6 +6,10 @@ function Navbar() {
   const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
   const [visible, setVisible] = useState(true);
 
+  const classNav = 'fixed top-0 left-0 right-0 flex justify-around text-black text-[1.2rem] py-[1.5rem] px-[5%] z-50 transition-all ease-in-out  duration-500 backdrop-blur-xl';
+
+  const isNavActive = 'translate-y-[-100px]';
+
   const handleScroll = () => {
     const currentScrollPos = window.scrollY;
     const isVisible = prevScrollPos > currentScrollPos;
@@ -32,31 +36,49 @@ function Navbar() {
   }, [prevScrollPos]);
 
   return (
-    <>
-      <nav className={`${visible ? "" : "active"} group`}>
-        <div className="logo">
-          <a href={"#"}> UI LOVER</a>
-        </div>
-
-        <div className={`list-link ${isActive ? "active" : ""} font-light`}>
-          <a href={"#menu"} className={`hover:text-blue-500`}>
-            Menu
-          </a>
-          <a href={`#food`}>Food</a>
-          <a href={`#service`}>Services</a>
-          <a href={`#about`}>About Us</a>
-        </div>
-        <div className="icon-buy block md:flex md:gap-4 relative md:content-center ">
-          <a className="  w-10 h-10  rounded-full relative  flex justify-center">
-            <Icon.Search className="absolute bottom-2" />
-          </a>
-          <a className="bg-white  w-10 h-10 shadow-md rounded-full relative  flex justify-center  ">
-            <Icon.ShoppingBag className="absolute bottom-2" />
-          </a>
-        </div>
+      <nav className={`${visible ? "" : isNavActive} ${classNav}`}>
+        <Logo />
+        <ListMenu isActive={isActive}/>
+        <SearchAndCart />
       </nav>
-    </>
   );
+}
+
+
+function Logo () {
+ return (
+<div className="text-[1.2rem] uppercase">
+    <a href={"#"}> UI LOVER</a>
+</div>
+ )
+}
+
+const ListMenu = ({isActive}) => {
+
+  return (
+    <div className={`ms-[20px] flex gap-[50px] ${isActive ? "active" : ""} font-light`}>
+    <a href={"#menu"} className={`hover:text-blue-500`}>
+      Menu
+    </a>
+    <a href={`#food`} className={`hover:text-blue-500`} >Food</a>
+    <a href={`#service`} className={`hover:text-blue-500`}>Services</a>
+    <a href={`#about`} className={`hover:text-blue-500`}>About Us</a>
+  </div>
+  )
+}
+
+
+const SearchAndCart = () => {
+  return (
+    <div className="block md:flex md:gap-4 relative md:content-center ">
+    <a className="w-10 h-10  rounded-full relative  flex justify-center">
+      <Icon.Search className="absolute bottom-2" />
+    </a>
+    <a className="bg-white  w-10 h-10 shadow-md rounded-full relative  flex justify-center  ">
+      <Icon.ShoppingBag className="absolute bottom-2" />
+    </a>
+  </div>
+  )
 }
 
 export default Navbar;
