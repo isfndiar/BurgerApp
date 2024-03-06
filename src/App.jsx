7c7c5baa4c8a9b3ddedd4f5f7e27b86335007cb8
Isfandiar Adi <141855809+isfndiar@users.noreply.github.com>
2024-03-07 00:12:07 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./redux/Store";
 function App() {
   return (
     <>
